92e04f3d7bd2907eb794802fc81a0682b2b17613
Zer0Tym3 <45374225+Zer0Tym3@users.noreply.github.com>
2024-03-03 19:49:06 -0500

    After Assistance from GIT

---
+++ AfterGitAssist/ServerNames.java
@@ -0,0 +1,32 @@
+public class ServerNames {
+    private static String routerName = "Zer0s-PC";
+    private static String serverAddress = "192.168.1.87";
+    private static String clientAddress = "192.168.1.76";
+    private static String textFile = "C:\\Temp\\file.txt";
+    private static String vidFile = "C:\\Temp\\drop.avi";
+    private static String vidFile2 = "C:\\Temp\\Video.mp4";
+
+
+    public static String getRouterName() {
+        return routerName;
+    }
+
+    public static String getServerAddress() {
+        return serverAddress;
+    }
+
+    public static String getClientAddress() {
+        return clientAddress;
+    }
+
+    public static String getTextFile() {
+        return textFile;
+    }
+
+    public static String getVidFile() {
+        return vidFile;
+    }
+    public static String getVidFile2() {
+        return vidFile2;
+    }
+}
